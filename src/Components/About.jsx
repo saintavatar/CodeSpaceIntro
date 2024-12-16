708c6487e872483c48951d3e40c84f839a418b12
Joseph Roberts <143056065+saintavatar@users.noreply.github.com>
2024-12-16 08:19:06 +0000

    set homepage for github pages deploy

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Computer Science student studying at Southern New Hampshire University. I enjoy learning new things and ave a natural curiousity about technology.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
+  "Python",
   "User experience",
   "Inclusive design",
   "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Networking",
+  "Security",
 ];
 
 /**
