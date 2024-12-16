e278a4de402185b9d06981a5733e592f4f396f6b
Joseph Roberts <143056065+saintavatar@users.noreply.github.com>
2024-12-16 08:19:40 +0000

    Updates

--- index.975ef6c8.js
+++ index.975ef6c8.js
@@ -27221,7 +27221,7 @@ const imageAltText = "purple and blue abstract background";
  * Use this to give more information about what you are passionate about,
  * how you best work, or even a quote. This will help someone learn more
  * about you on a professional level.
- */ const detailOrQuote = "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+ */ const detailOrQuote = "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my hands-on experience I continually look for new and better ways to make tech accessible by all.";
 const About = ()=>{
     return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("section", {
         className: "padding",
