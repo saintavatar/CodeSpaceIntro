4b983dd6d885526b050c21102a598189575d1ff8
Joseph Roberts <143056065+saintavatar@users.noreply.github.com>
2024-12-17 10:09:18 +0000

    change txt to center align

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -65,7 +65,7 @@ const About = () => {
         <hr />
         <ul
           style={{
-            textAlign: "left",
+            textAlign: "center",
             columns: 1,
             fontSize: "1.25rem",
             margin: "2rem 3rem",
